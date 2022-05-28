7cfd84ef6847259a52cad0a8fb5841c76aa13e5e
Manoj AP <manojap.nair@gmail.com>
2022-05-28 21:56:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import SimpleEmojiEditor from "./Editor";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi I'am a Draft-js Plugin</h1>
+      <SimpleEmojiEditor />
+    </div>
+  );
+}
